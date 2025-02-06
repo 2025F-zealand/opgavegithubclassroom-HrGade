3e0cc5d5a710f169822f3d02cda62ac2d73e1282
Adam Gade-Frimann <adg001@edu.zealand.dk>
2025-02-06 12:43:30 +0100

    Tilføjet "demo"-klasse m. tilhørende proporties

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        public string Name;
+
+        public int føldelsår;
+
+        public double PengePåKonto;
+    }
+}
